de970ca4f29fbc7286f484c6e539da34f991b737
Dmytro Ogirenko <dmytro.ogirenko@thea-auto.com>
2020-08-18 22:54:02 +0300

    add observe reified function; add searchUsers viewModel function;

--- app/src/main/java/com/example/facebooktest/base/BaseBindingActivity.kt
+++ app/src/main/java/com/example/facebooktest/base/BaseBindingActivity.kt
@@ -1,9 +1,15 @@
 package com.example.facebooktest.base
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import javax.inject.Inject
 
 abstract class BaseActivity<VM : BaseActivityViewModel<V>, V: MxView> : BaseInjectorActivity() {
 
     @Inject lateinit var viewModel: VM
+
+    protected inline fun <reified T> LiveData<T>.observe(noinline block: (t: T) -> Unit) {
+        observe(this@BaseActivity, Observer<T>(block))
+    }
 
 }
--- app/src/main/java/com/example/facebooktest/features/main/MainViewModel.kt
+++ app/src/main/java/com/example/facebooktest/features/main/MainViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.facebooktest.features.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.facebooktest.base.BaseActivityViewModel
-import com.example.facebooktest.base.MxView
+import com.example.facebooktest.entity.response.SearchUserEntity
+import com.example.facebooktest.repository.SearchRepository
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposables
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(): BaseActivityViewModel<MainView>() {
+class MainViewModel @Inject constructor(private val repository: SearchRepository): BaseActivityViewModel<MainView>() {
+
+    private val _searchUsers = MutableLiveData<List<SearchUserEntity>>()
+    private val disposable = CompositeDisposable()
+
+    fun searchUsers(query: String): LiveData<List<SearchUserEntity>> {
+        repository.searchUsers(query)
+            .subscribe({
+                _searchUsers.value = it.items
+            }, {
+                it.printStackTrace()
+            })
+            .let(disposable::add)
+        return _searchUsers
+    }
 
 }
