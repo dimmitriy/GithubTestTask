f6c85593d6a8af0b35d6b143d0906990771ef5b5
Dmytro Ogirenko <dmytro.ogirenko@thea-auto.com>
2020-08-18 10:53:37 +0300

    add necessary gradle dependencies; change gradle plugin version; add const file;

---
+++ app/src/main/java/com/example/facebooktest/Const.kt
@@ -0,0 +1,12 @@
+package com.example.facebooktest
+
+class Const {
+
+    companion object {
+        const val CONNECTION_TIMEOUT: Int = 30
+        const val READ_TIMEOUT: Int = 30
+        const val WRITE_TIMEOUT: Int = 30
+        const val API_URL: String = "https://api.github.com"
+    }
+
+}
