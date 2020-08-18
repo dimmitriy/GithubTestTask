94f2deb3c2d5326fd1fc5c4639f08e1ddd1c1926
Dmytro Ogirenko <dmytro.ogirenko@thea-auto.com>
2020-08-18 10:55:44 +0300

    add dagger scopes; add retrofit converter DateTimeTypeConverter; add RxSingleSchedulers, GitHubApiService; add App class with dagger initialization;

---
+++ app/src/main/java/com/example/facebooktest/api/DateTimeTypeConverter.kt
@@ -0,0 +1,24 @@
+package com.example.facebooktest.api
+
+import com.google.gson.JsonDeserializationContext
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonElement
+import com.google.gson.JsonParseException
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializationContext
+import com.google.gson.JsonSerializer
+import org.joda.time.DateTime
+import org.joda.time.DateTimeZone
+import java.lang.reflect.Type
+
+class DateTimeTypeConverter : JsonSerializer<DateTime>, JsonDeserializer<DateTime> {
+
+    override fun serialize(src: DateTime, srcType: Type, context: JsonSerializationContext
+    ): JsonElement = JsonPrimitive(src.toString())
+
+    @Throws(JsonParseException::class)
+    override fun deserialize(json: JsonElement, type: Type,
+        context: JsonDeserializationContext
+    ): DateTime = DateTime(json.asString, DateTimeZone.UTC)
+
+}
